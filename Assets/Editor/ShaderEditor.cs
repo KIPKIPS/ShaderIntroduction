4b7e1331ee2057da9ed82553595f40ba0201909d
RECNEPS-KIPS <2432719656@qq.com>
2020-01-05 16:21:58 +0800

    Unity编写Shader调用SublimeText编辑器
    1.Unity Editor导入UnityShader脚本
    2.配置Sublime Text的环境变量SublimeText_Path(变量值为Sublime Text的安装路径)
    3.重启Windows资源管理器
    4.导入插件包,Sublime Text Preferences /Browse Packages,将zip文件解压到该目录下,重命名文件夹为Unity-Shader
    5.修改文件UnityShader.sublime-settings->
    {
        //must set the path and version  u5 or u4
        "Unity_Version":"U5",
        "Shader_path": "K:/Unity/Editor/Data/CGIncludes",
        "U5_Shader_path":"K:/Unity/Editor/Data/CGIncludes"
    }
    6.修改路径

---
+++ Assets/Editor/ShaderEditor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+public class LuaTxtEditor {
+
+    //http://www.xuanyusong.com/archives/3702 
+    [UnityEditor.Callbacks.OnOpenAssetAttribute(1)]
+    public static bool step1(int instanceID, int line) {
+        return false;
+    }
+
+    [UnityEditor.Callbacks.OnOpenAssetAttribute(2)]
+    public static bool step2(int instanceID, int line) {
+        string strFilePath = AssetDatabase.GetAssetPath(EditorUtility.InstanceIDToObject(instanceID));
+        string strFileName = System.IO.Directory.GetParent(Application.dataPath) + "/" + strFilePath;
+
+        if (strFileName.EndsWith(".shader")) {
+            string strSublimeTextPath = Environment.GetEnvironmentVariable("SublimeText_Path");
+            if (strSublimeTextPath != null && strSublimeTextPath.Length > 0) {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = strSublimeTextPath + (strSublimeTextPath.EndsWith("/") ? "" : "/") + "sublime_text.exe";
+                startInfo.Arguments = "\"" + strFileName + "\"";
+                process.StartInfo = startInfo;
+                process.Start();
+
+                //Debug.Log(startInfo.FileName + " \t " + startInfo.Arguments);
+
+                return true;
+            }
+            else {
+                Debug.Log("Not Found Enviroment Variable 'SublimeText_Path'.");
+
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+}

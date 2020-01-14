b1de5da1ce17ec1c937f869360eca56c4f8dcad4
RECNEPS-KIPS <2432719656@qq.com>
2020-01-14 17:07:41 +0800

    实现缩放

--- Assets/Scripts/MVPTransform.cs
+++ Assets/Scripts/MVPTransform.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,15 +15,24 @@ public class MVPTransform : MonoBehaviour
     void Update() {
         //绕Y轴做旋转矩阵RM
         //对RM矩阵赋值
-        Matrix4x4 RM=new Matrix4x4();
-        RM[0, 0] = Mathf.Cos(Time.realtimeSinceStartup);
-        RM[0, 2] = Mathf.Sin(Time.realtimeSinceStartup);
-        RM[1, 1] = 1;
-        RM[2, 0] = -Mathf.Sin(Time.realtimeSinceStartup);
-        RM[3, 3] = 1;
+        Matrix4x4 rotateMatrix = new Matrix4x4();
+        rotateMatrix[0, 0] = Mathf.Cos(Time.realtimeSinceStartup);//根据微分时间片更改旋转角度
+        rotateMatrix[0, 2] = Mathf.Sin(Time.realtimeSinceStartup);
+        rotateMatrix[1, 1] = 1;
+        rotateMatrix[2, 0] = -Mathf.Sin(Time.realtimeSinceStartup);
+        rotateMatrix[3, 3] = 1;
+
+        //缩放矩阵
+        Matrix4x4 scaleMatrix=new Matrix4x4();
+        scaleMatrix[0, 0] = Mathf.Sin(Time.realtimeSinceStartup)/4+0.5f;
+        scaleMatrix[1, 1] = Mathf.Cos(Time.realtimeSinceStartup)/8+0.5f;
+        scaleMatrix[2, 2] = Mathf.Sin(Time.realtimeSinceStartup)/6+0.5f;
+        scaleMatrix[3, 3] = 1;
+
         //相机投影矩阵 * 世界-->相机矩阵 * 物体自身的位置信息矩阵-->世界矩阵
         Matrix4x4 mvp = Camera.main.projectionMatrix * Camera.main.worldToCameraMatrix * transform.localToWorldMatrix ;//*RM
         GetComponent<Renderer>().material.SetMatrix("mvp",mvp);//获取Shader文件中mvp矩阵的值
-        GetComponent<Renderer>().material.SetMatrix("rm", RM);//获取Shader文件中rm矩阵的值
+        //GetComponent<Renderer>().material.SetMatrix("rotateMatrix", rotateMatrix);//获取Shader文件中rm矩阵的值
+        GetComponent<Renderer>().material.SetMatrix("scaleMatrix", scaleMatrix);
     }
 }
--- Assets/Shader/VertexShader_06.shader
+++ Assets/Shader/VertexShader_06.shader
@@ -10,7 +10,8 @@
             #pragma fragment Frag 
 
              float4x4 mvp;
-             float4x4 rm;
+             float4x4 rotateMatrix;
+             float4x4 scaleMatrix;
             //通信结构体
             struct VertToFrag{
                 float4 pos:POSITION ;
@@ -20,12 +21,13 @@
                 VertToFrag vtf;
                 //float4x4 m=mul(mvp,rm);//先将UNITY_MATRIX_MVP和变换矩阵相乘
                 float4x4 unityMVP=UNITY_MATRIX_MVP;//先将UNITY_MATRIX_MVP矩阵赋值给新矩阵m,否则Unity会自动将mul函数替换成UnityObjectToClipPos函数
-                float4x4 m=mul(unityMVP,rm);
+                //float4x4 rTempMatrix=mul(unityMVP,rotateMatrix);
+                float4x4 sTempMatrix=mul(unityMVP,scaleMatrix);
                 //得到变换矩阵在屏幕空间的投影位置
 
                 //vtf.pos=mul(UNITY_MATRIX_MVP,adb.vertex)
                 //vtf.pos=UnityObjectToClipPos(adb.vertex );
-                vtf.pos=mul(m,adb.vertex);
+                vtf.pos=mul(sTempMatrix,adb.vertex);
                 return vtf;
             }
             float4 Frag():COLOR{
